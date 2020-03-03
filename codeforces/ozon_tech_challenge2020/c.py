48edb456e435d93a95da5f75334836bb8c406eb2
Patrick <patrick@quotabook.com>
2020-03-04 03:37:46 +0900

    ac.

--- codeforces/ozon_tech_challenge2020/c.py
+++ codeforces/ozon_tech_challenge2020/c.py
@@ -2,7 +2,14 @@ import sys
 
 n, m = list(map(int, sys.stdin.readline().rstrip().split()))
 a = list(map(int, sys.stdin.readline().rstrip().split()))
-a.sort()
-diff = []
-for i in range(n - 1):
-    diff.append(a[i + 1] - a[i])
+
+if n <= m:
+    re = 1
+    for i in range(n):
+        for j in range(i + 1, n):
+            re *= abs(a[i]-a[j])
+            if re > m:
+                re %= m
+    print(re % m)
+else:
+    print(0)
